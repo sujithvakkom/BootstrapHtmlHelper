88d798771bd8d77598db4ce1003f2fab9b5dc8b0
Sujith Radhakrishnan <sujithvakkom@gmail.com>
2016-08-13 17:05:04 +0400

    BootStrapHelper 1.0.0
    BootStrapHelper 1.0.0

---
+++ MyExtentions.AdminLTEPasswordFor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace BootstrapHtmlHelper
+{
+    public static partial class MyExtentions
+    {
+        /// <summary>
+        /// <div class="form-group has-feedback">
+        ///     <input type="email" class="form-control" placeholder="Email">
+        ///     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        /// </div>
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="htmlHelper"></param>
+        /// <param name="expression"></param>
+        /// <param name="htmlLabelAttributes"></param>
+        /// <param name="htmlTextBoxAttributes"></param>
+        /// <param name="htmlGroupAttributes"></param>
+        /// <param name="showLabel"></param>
+        /// <param name="hasValidation"></param>
+        /// <returns></returns>
+        public static IHtmlString AdminLTEPasswordFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression,
+            IDictionary<string, object> htmlLabelAttributes,
+            IDictionary<string, object> htmlTextBoxAttributes,
+            IDictionary<string, object> htmlGroupAttributes,
+            IDictionary<string, object> htmlGlyphiconsAttributes,
+            String glyphiconsName,
+            bool showLabel = false,
+            bool hasValidation = true,
+            bool showGlyphicons = false)
+        {
+
+            var formGroup = new TagBuilder("div");
+            if (htmlGroupAttributes == null)
+                formGroup.AddCssClass("form-group has-feedback");
+            else
+                foreach (var attribute in htmlGroupAttributes)
+                {
+                    formGroup.Attributes.Add(attribute.Key, attribute.Value.ToString());
+                }
+
+            if (htmlTextBoxAttributes == null) htmlTextBoxAttributes = new Dictionary<String, object>();
+
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression != null)
+            {
+                var x = memberExpression.Member;
+                var y = x.GetAttribute<DisplayAttribute>();
+                htmlTextBoxAttributes.Add(new KeyValuePair<string, object>("placeholder", y.Name));
+            }
+            //htmlTextBoxAttributes.Add(new KeyValuePair<string, object>("placeholder", expression.Name));
+
+            var textbox = htmlHelper.PasswordFor(expression, htmlTextBoxAttributes);
+
+            var validationSummary = htmlHelper.ValidationMessageFor(expression);
+
+
+            ///     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            var spanGlyphicons = new TagBuilder("span");
+            spanGlyphicons.AddCssClass("fa");
+            spanGlyphicons.AddCssClass("form-control-feedback");
+            spanGlyphicons.AddCssClass(glyphiconsName);
+
+            if (!(htmlGlyphiconsAttributes == null))
+                foreach (var attribute in htmlGlyphiconsAttributes)
+                {
+                    formGroup.Attributes.Add(attribute.Key, attribute.Value.ToString());
+                }
+
+            //if (hasValidation) formGroup.AddCssClass("has-warning");
+
+            formGroup.InnerHtml = (showLabel ? htmlHelper.LabelFor(expression, htmlLabelAttributes).ToHtmlString() : "") +
+                textbox.ToHtmlString() +
+                (hasValidation ? validationSummary.ToHtmlString() : "") +
+                (showGlyphicons ? spanGlyphicons.ToString() : "");
+            /*formGroup.SetInnerText(
+                (showLabel?htmlHelper.LabelFor(expression,htmlLabelAttributes).ToHtmlString():"")+
+                textbox.ToHtmlString()
+                );
+            */
+            return MvcHtmlString.Create(formGroup.ToString());
+        }
+    }
+}
---
+++ MyExtentions.BootstrapPasswordFor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace BootstrapHtmlHelper
+{
+    public static partial class MyExtentions
+    {
+        /// <summary>
+        /// Output:
+        /// <div class="form-group">
+        ///         <label>First Name</label>
+        ///         <input class="form-control" data-val="true" data-val-required="The First Name field is required." id="Name" name="Name" placeholder="Description" value="" type="text">
+        ///     </div>
+        /// </summary>
+        /// <typeparam name="TModel">Expression for Model</typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="htmlHelper"></param>
+        /// <param name="expression"></param>
+        /// <param name="showLabel"></param>
+        /// <param name="htmlLabelAttributes"></param>
+        /// <param name="htmlTextBoxAttributes"></param>
+        /// <returns></returns>
+        public static IHtmlString BootstrapPasswordFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression,
+            IDictionary<string, object> htmlLabelAttributes,
+            IDictionary<string, object> htmlTextBoxAttributes,
+            IDictionary<string, object> htmlGroupAttributes,
+            bool showLabel = true,
+            bool hasValidation = true)
+        {
+
+            var formGroup = new TagBuilder("div");
+            if (htmlGroupAttributes == null)
+                formGroup.AddCssClass("form-group");
+            else
+                foreach (var attribute in htmlGroupAttributes)
+                {
+                    formGroup.Attributes.Add(attribute.Key, attribute.Value.ToString());
+                }
+
+            if (htmlTextBoxAttributes == null) htmlTextBoxAttributes = new Dictionary<String, object>();
+
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression != null)
+            {
+                var x = memberExpression.Member;
+                var y = x.GetAttribute<DisplayAttribute>();
+                htmlTextBoxAttributes.Add(new KeyValuePair<string, object>("placeholder", y.Name));
+            }
+            //htmlTextBoxAttributes.Add(new KeyValuePair<string, object>("placeholder", expression.Name));
+
+            var textbox = htmlHelper.TextBoxFor(expression, htmlTextBoxAttributes);
+
+            var validationSummary = htmlHelper.ValidationMessageFor(expression);
+
+            //if (hasValidation) formGroup.AddCssClass("has-warning");
+
+            formGroup.InnerHtml = (showLabel ? htmlHelper.LabelFor(expression, htmlLabelAttributes).ToHtmlString() : "") +
+                textbox.ToHtmlString() +
+                (hasValidation ? validationSummary.ToHtmlString() : "");
+            /*formGroup.SetInnerText(
+                (showLabel?htmlHelper.LabelFor(expression,htmlLabelAttributes).ToHtmlString():"")+
+                textbox.ToHtmlString()
+                );
+            */
+            return MvcHtmlString.Create(formGroup.ToString());
+        }
+
+        public static T GetAttribute<T>(this ICustomAttributeProvider provider)
+        where T : Attribute
+        {
+            var attributes = provider.GetCustomAttributes(typeof(T), true);
+            return attributes.Length > 0 ? attributes[0] as T : null;
+        }
+    }
+}
