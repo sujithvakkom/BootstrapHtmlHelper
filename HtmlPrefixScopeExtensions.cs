f82b1243ad8f07d051767b4dc8214345d63d667e
Sujith Radhakrishnan <sujithvakkom@gmail.com>
2018-04-29 17:46:03 +0400

    Paging

---
+++ HtmlPrefixScopeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace BootstrapHtmlHelper
+{
+    public static class HtmlPrefixScopeExtensions
+    {
+        public static IDisposable BeginPrefixScope(this HtmlHelper html, string htmlFieldPrefix)
+        {
+            return new HtmlFieldPrefixScope(html.ViewData.TemplateInfo, htmlFieldPrefix);
+        }
+    }
+}
