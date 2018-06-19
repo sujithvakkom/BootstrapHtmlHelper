7ae609a1968175a33cb8bc6d30eb9eb8cece0761
Sujith Radhakrishnan <sujithvakkom@gmail.com>
2018-06-19 18:09:59 +0400

    Data

--- ChartJs/data.cs
+++ ChartJs/data.cs
@@ -21,22 +21,35 @@ namespace BusinessExcel.Models.ChartJs
             //Expression<Func<TSource, TResult>> selector
             labels =  source.Select(YAxixsLabelField).Distinct().ToArray();
             datasets = new List<dataset>();
-            /*
             foreach (var label in labels)
                 datasets.Add(
                     new dataset(label,
-                    source.Where(yAxixsLabelField.).Select(selector).ToArray()
+                    source.Where(
+                        EqualToExpression(yAxixsLabelField,label)
+                        ).Select(selector).ToArray()
                     ));
-                    */
+
         }
-        public data(IQueryable<T> source, Expression<Func<T, String>> idField, Expression<Func<T, String>> stageField, , Expression<Func<T, decimal>> selector)
+        public data(IQueryable<T> source, Expression<Func<T, String>> idField, Expression<Func<T, String>> stageField, Expression<Func<T, decimal>> selector)
         {
             this.dataSource = source;
             this.YAxixsLabelField = idField;
             this.Selection = selector;
             //Expression<Func<TSource, TResult>> selector
             labels = source.Select(YAxixsLabelField).Distinct().ToArray();
+var stages = source.Select(stageField).Distinct().ToArray()
+/*
+            foreach (var label in labels)
+            {
+                foreach (var stage in stages)
+                var dS = new dataset(label,stage,
 
+                    source.Where(
+                        EqualToExpression(yAxixsLabelField, label)
+                        ).where(.Select(selector).ToArray()
+                    )
+            }
+            */
         }
         /// <summary>
         /// X Axis labels
@@ -44,5 +57,15 @@ namespace BusinessExcel.Models.ChartJs
         public string[] labels { get; set; }
 
         public List<dataset> datasets { get; set; }
+
+       private Expression<Func<TSource, bool>> EqualToExpression<TSource, TValue>(
+    Expression<Func<TSource, TValue>> selectValue, TValue targetValue)
+        {
+            return Expression.Lambda<Func<TSource, bool>>(
+                Expression.Equal(
+                    selectValue.Body,
+                    Expression.Constant(targetValue)),
+                selectValue.Parameters);
+        }
     }
 }
