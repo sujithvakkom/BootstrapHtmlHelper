cd1429121cf7b71c5b03d03d5f200017e427cde7
Sujith Radhakrishnan <sujithvakkom@gmail.com>
2018-07-02 18:08:30 +0400

    Error

---
+++ ModelStateDictionary.GetErrors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace BootstrapHtmlHelper
+{
+    public static class Extention
+    {
+        public static string GetErrors(this ModelStateDictionary values)
+        {
+            string ModelErrors = "";
+            foreach (ModelState modelState in values.Values)
+            {
+                foreach (ModelError error in modelState.Errors)
+                {
+                    ModelErrors = ModelErrors == "" ? error.ErrorMessage :
+                        ModelErrors + Environment.NewLine + error.ErrorMessage;
+                }
+            }
+            return ModelErrors;
+        }
+    }
+}
